57a0588697397768882e288f814f5545a2e3721e
ghazalin1 <44950615+ghazalin1@users.noreply.github.com>
2020-12-23 14:20:17 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import pic from './pic.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={pic} className="App-logo" alt="logo" />
+        <p>
+          Run on desktop <code>Then Deploy on GitHub</code> Then download repository directly and Run Again
+        </p>
+       
+          Happy Coding Again! Depolyment Solutions
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
